24c4922dcde0c52b25f4361ba74c53c4dfac1b86
DMaximoVarela <maxivarela95@gmail.com>
2025-01-20 14:42:04 -0300

    feat: use new theme from ui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { ThemeProvider } from "@mui/material";
-import theme from "./theme";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 import Router from "./routes";
+import theme from "./theme";
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <Router />
       </ThemeProvider>
     </>
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,8 +1,10 @@
 import { createTheme } from "@mui/material";
-import breakpoints from "./breakpoints";
+import { react_chrono_ts_theme } from "@react-chrono-ts/ui";
 
-const theme = createTheme({
-  ...breakpoints,
-});
+const mode: "light" | "dark" = "light";
+
+console.log(react_chrono_ts_theme(mode));
+
+const theme = createTheme(react_chrono_ts_theme(mode));
 
 export default theme;

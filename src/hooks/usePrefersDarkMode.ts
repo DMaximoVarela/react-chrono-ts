852addeacc8ee5951b915aa67d70a3c4f552e2a9
DMaximoVarela <maxivarela95@gmail.com>
2025-03-30 13:34:05 -0300

    feat: add hook usePrefersDarkMode

---
+++ src/hooks/usePrefersDarkMode.ts
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+const usePrefersDarkMode = () => {
+  const [isDarkMode, setIsDarkMode] = useState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    const handleChange = () => setIsDarkMode(mediaQuery.matches);
+
+    mediaQuery.addEventListener("change", handleChange);
+    return () => mediaQuery.removeEventListener("change", handleChange);
+  }, []);
+
+  return isDarkMode ? "dark" : "light";
+};
+
+export default usePrefersDarkMode;

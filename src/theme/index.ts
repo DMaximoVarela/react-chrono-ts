bd6cd6a2ad4915fec23872b99304d1cd1e337483
DMaximoVarela <maxivarela95@gmail.com>
2025-03-30 13:35:52 -0300

    chore: remove theme file

--- src/theme/index.ts
+++
@@ -1,8 +0,0 @@
-import { createTheme } from "@mui/material";
-import { react_chrono_ts_theme } from "@react-chrono-ts/ui";
-
-const mode: "light" | "dark" = "dark";
-
-const theme = createTheme(react_chrono_ts_theme(mode));
-
-export default theme;

15ff329e6766e8060c36ade2a94d0fa0400bdd09
DMaximoVarela <maxivarela95@gmail.com>
2025-03-30 22:06:49 -0300

    feat: add new component DigitalClock

--- src/components/Layout/style.ts
+++ src/components/Layout/style.ts
@@ -3,13 +3,13 @@ import { styled } from "@mui/material";
 export const StyledMain = styled("main")(({ theme }) => ({
   height: "100vh",
   flexGrow: 1,
-  paddingTop: "24px",
+  paddingTop: "1rem",
   [theme.breakpoints.up("sm")]: {
     paddingLeft: "96px",
   },
   [theme.breakpoints.down("sm")]: {
     paddingLeft: "12px",
-    paddingTop: "60px",
+    paddingTop: "5em",
   },
   [theme.breakpoints.up("md")]: {
     paddingLeft: "260px",

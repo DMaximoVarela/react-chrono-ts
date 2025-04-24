6fe79a45f1a10982fa0573a6644ae99edc6ff3b4
DMaximoVarela <maxivarela95@gmail.com>
2025-04-23 21:39:10 -0300

    feat: add timezone config

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -3,24 +3,32 @@ import {
   Dialog,
   DialogContent,
   DialogTitle,
+  FormControl,
   Grid2,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
   ToggleButton,
   ToggleButtonGroup,
   Typography,
 } from "@mui/material";
 import useSettingsStore from "@Stores/settingsStore";
+import { TIME_ZONES } from "@Utils/timezoneUtils";
 
 const Settings = () => {
   const open = useSettingsStore((state) => state.open);
   const setOpen = useSettingsStore((state) => state.setOpen);
   const mode = useSettingsStore((state) => state.mode);
   const setMode = useSettingsStore((state) => state.setMode);
+  const timezone = useSettingsStore((state) => state.timezone);
+  const setTimezone = useSettingsStore((state) => state.setTimezone);
 
   const handleClose = () => {
     setOpen(false);
   };
 
-  const handleChange = (
+  const handleMode = (
     _event: React.MouseEvent<HTMLElement>,
     newMode: "light" | "system" | "dark"
   ) => {
@@ -30,13 +38,18 @@ const Settings = () => {
     setMode(newMode);
   };
 
+  const handleTimezone = (event: SelectChangeEvent<string>) => {
+    const selected = event.target.value;
+    setTimezone(selected);
+  };
+
   return (
     <Dialog open={open} onClose={handleClose} fullWidth maxWidth="md">
       <DialogTitle>Configuración</DialogTitle>
       <DialogContent>
         <Grid2 container flexDirection="column">
-          <Typography>MODE</Typography>
-          <ToggleButtonGroup value={mode} exclusive onChange={handleChange}>
+          <Typography>Modo</Typography>
+          <ToggleButtonGroup value={mode} exclusive onChange={handleMode}>
             <ToggleButton value="light">
               <Grid2 container spacing={1}>
                 <LightMode /> <Typography variant="button">Light</Typography>
@@ -53,6 +66,27 @@ const Settings = () => {
               </Grid2>
             </ToggleButton>
           </ToggleButtonGroup>
+        </Grid2>
+        <Grid2 container flexDirection="column">
+          <Typography>Timezone</Typography>
+          <Grid2 container spacing={2}>
+            <FormControl fullWidth>
+              <InputLabel id="timezone-select-label">Timezone</InputLabel>
+              <Select
+                labelId="timezone-select-label"
+                id="timezone-select"
+                value={timezone}
+                label="Timezone"
+                onChange={handleTimezone}
+              >
+                {TIME_ZONES.map((timezone) => (
+                  <MenuItem key={timezone.id} value={timezone.name}>
+                    {timezone.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid2>
         </Grid2>
       </DialogContent>
     </Dialog>

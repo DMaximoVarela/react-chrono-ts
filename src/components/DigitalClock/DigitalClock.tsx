a46ab2bf77b781aa4125ffb50c15e5cef8a61790
DMaximoVarela <maxivarela95@gmail.com>
2025-05-02 13:25:21 -0300

    style: some styles changes

--- src/components/DigitalClock/DigitalClock.tsx
+++ src/components/DigitalClock/DigitalClock.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { DateTime } from "luxon";
-import { Box, Typography, useMediaQuery } from "@mui/material";
+import { Grid2, Typography } from "@mui/material";
 import { Edit } from "@mui/icons-material";
 import useSettingsStore from "@Stores/settingsStore";
 import { removeFormattedTimezone } from "@Utils/timezoneUtils";
@@ -22,31 +22,41 @@ const DigitalClock = () => {
   }, [timezone]);
 
   const date = time.setLocale("es").toFormat("EEEE, d 'de' LLLL 'de' yyyy");
-
-  const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
-
   return (
     <>
-      <Box
-        sx={{
-          width: "100%",
-          background: (theme) => theme.palette.background.paper,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: isSmall ? "2em 1em" : "2em",
-          borderRadius: ".5rem",
-        }}
+      <Grid2
+        container
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={0}
       >
+        <Typography
+          variant="h1"
+          component="h1"
+          fontSize={{ xs: "5rem", md: "6.5rem", lg: "8rem" }}
+          fontWeight="bold"
+          color="primary"
+        >
+          {time.toFormat("HH:mm:ss")}
+        </Typography>
         <Typography
           variant="body1"
           component="span"
-          fontSize={isSmall ? "1em" : "1.5em"}
+          fontSize={{ xs: "1rem", md: "1.5rem" }}
+          color="text.primary"
+        >
+          {date.charAt(0).toUpperCase() + date.slice(1)}
+        </Typography>
+        <Typography
+          variant="body1"
+          component="span"
+          fontSize={{ xs: ".75rem", md: "1rem" }}
           sx={{
             cursor: "pointer",
+            color: "text.primary",
             "&:hover": {
-              color: "primary.main",
+              color: "text.secondary",
             },
             transition: "all 200ms ease-out",
           }}
@@ -54,18 +64,7 @@ const DigitalClock = () => {
         >
           {timezone.replace("_", " ").replace("/", ", ")} <Edit />
         </Typography>
-        <Typography variant="h1" component="h1" fontSize="3.5em">
-          {time.toFormat("HH:mm:ss")}
-        </Typography>
-        <Typography
-          variant="body1"
-          component="span"
-          color="primary"
-          fontSize={isSmall ? "1em" : "1.5em"}
-        >
-          {date.charAt(0).toUpperCase() + date.slice(1)}
-        </Typography>
-      </Box>
+      </Grid2>
     </>
   );
 };
--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,14 +1,10 @@
 import DigitalClock from "@Components/DigitalClock";
 import Layout from "@Components/Layout";
-import { Title } from "@react-chrono-ts/ui";
 
 const Landing = () => {
   return (
     <Layout>
       <div style={{ margin: "0 1em 2.5em 1em" }}>
-        <Title text="¡Bienvenido/a! Organiza tu tiempo de manera inteligente." />
-      </div>
-      <div style={{ margin: "1em" }}>
         <DigitalClock />
       </div>
     </Layout>
